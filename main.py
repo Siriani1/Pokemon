8a3909b2cf0aa762e840148e287ff2d1b1732029
Sirio <siriani.andrea@iisgalvanimi.edu.it>
2021-12-13 18:27:53 +0100

    punto 4

--- main.py
+++ main.py
@@ -10,10 +10,44 @@ app = Flask(__name__)
 
 CORS(app)
 
+@app.route("/")
+def index():
+    return "Hello world!"
 
 @app.route("/all")
 def pokemon():
     return send_file('pokemon.json')
+
+@app.route("/pikachu")
+def pikachu():
+    f = open('pokemon.json') #Aprire il file json
+    data = json.load(f) #caricare l'oggetto dentro data
+    return data['pokemon'][random.randrange(0,13)] #estrarre una riga a caso dal vettore data
+    
+
+@app.route("/charmender")
+def charmender():
+    f = open('pokemon.json') #Aprire il file json
+    data = json.load(f) #caricare l'oggetto dentro data
+    return data['pokemon'][random.randrange(0,13)] #estrarre una riga a caso dal vettore data
+
+
+@app.route("/bulbasaur")
+def bulbasaur():
+    f = open('pokemon.json') #Aprire il file json
+    data = json.load(f) #caricare l'oggetto dentro data
+    return data['pokemon'][random.randrange(0,13)] #estrarre una riga a caso dal vettore data
+
+
+@app.route("/snorlax")
+def snorlax():
+    f = open('pokemon.json') #Aprire il file json
+    data = json.load(f) #caricare l'oggetto dentro data
+    return data['pokemon'][random.randrange(0,13)] #estrarre una riga a caso dal vettore data
+
+
+
+
 
 
 if __name__ == "__main__":
